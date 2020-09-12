2aed0f61afd8ff2cb9989bf4927629193067ac5c
Amanda Winkles <amandawinkles@gmail.com>
2020-09-11 23:06:05 -0500

    edited dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,12 @@ FROM node:12-alpine
 
 WORKDIR /usr/src/app
 
-ENV port=80
-
 COPY package.json ./
 
 RUN npm install
 
 COPY . .
 
-EXPOSE 80
+EXPOSE 5000
 
 CMD [ "node", "./server/index.js" ]
--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoUri = 'mongodb://localhost/images'; //db -> images
+const mongoUri = 'mongodb://localhost/images'; //db -> images //change this to connect to ec2 instance w/mongodb installed on the instance //to identify if db is running correctly  //if not firewall problem blocking the connection to the db or db isn't setup correctly on ec2 instance
 
 const db = mongoose.connect(mongoUri, {
   useNewUrlParser: true,
@@ -8,7 +8,7 @@ const db = mongoose.connect(mongoUri, {
 
 mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
 mongoose.connection.once('open', function() {
-  console.log('connected to db!');
+  console.log('connected to db!'); //once I get this log, i'll know db is good, then run seed script to seed db on instance
 });
 
 
